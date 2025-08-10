1adcfab56a9f3d76875bef9303644e18e4bd3a07
Mudheeru <ibrahimaqyl@gmail.com>
2025-08-11 00:33:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import sys
 import pygwalker
 from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+#from streamlit_pandas_profiling import st_profile_report
 
 
 st.set_page_config(layout="wide")
